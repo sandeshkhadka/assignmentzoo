fd741ced9c123084fe29fa9d255b3fe056009f6c
sandeshkhadka <pqrsandesh@gmail.com>
2025-05-14 22:15:23 +0545

    setup sqllite

--- app/core/config.py
+++ app/core/config.py
@@ -9,11 +9,13 @@ from starlette.datastructures import Secret
 config = Config(".env")
 
 API_PREFIX = "/api"
+FRONTEND_PREFIX = ""
 VERSION = "0.1.0"
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default=Secret(""))
 
 PROJECT_NAME: str = config("PROJECT_NAME", default="AssignmentZoo")
+DATABASE_URL = config("DATABASE_URL", default="sqlite:///./test.db")
 
 # logging configuration
 LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
--- app/core/events.py
+++ app/core/events.py
@@ -3,9 +3,11 @@ from typing import Callable
 import joblib
 from fastapi import FastAPI
 
-
+from app.db.base import Base, engine
 
 
 def create_start_app_handler(app: FastAPI) -> Callable:
     def start_app() -> None:
+        Base.metadata.create_all(bind=engine)
+
     return start_app
